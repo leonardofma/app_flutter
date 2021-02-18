5387dad3f9dc8991fb785f11e68ba9f2bd80ccde
Leonardo Fernandes <leonardo.fernandes@cybernet.local>
2021-02-18 14:37:08 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter PDF',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String url = "https://dnd5ed.github.io/livros/LDJ.pdf";
+  String pdfasset = "assets/sample.pdf";
+  PDFDocument _doc;
+  bool _loading;
+
+  @override
+  void initState() {
+    super.initState();
+    _initPdf();
+  }
+
+  _initPdf() async {
+    setState(() {
+      _loading = true;
+    });
+    final doc = await PDFDocument.fromURL(url);
+    setState(() {
+      _doc=doc;
+      _loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("PDF Student"),),
+      body: _loading ? Center(child: CircularProgressIndicator(),) : PDFViewer(document: _doc,
+      indicatorBackground: Colors.blue,
+      // showIndicator: false,
+      // showPicker: false,
+       ),
+    );
+  }
+}
